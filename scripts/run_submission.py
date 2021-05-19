39a2ea69989741b96b2caca5663087ebe37dfe79
Maximilian Zenk <m.zenk@dkfz-heidelberg.de>
2021-05-19 10:05:28 +0200

    Update scripts for testing and running containers

--- scripts/run_submission.py
+++ scripts/run_submission.py
@@ -11,6 +11,64 @@ import subprocess
 import time
 
 # TODO use logging instead of prints -> how to sensibly combine with FeTS-CLI?
+def run_container(sif, in_dir, out_dir, timeout_case):
+    num_cases = len(list(in_dir.iterdir()))
+
+    # These are defined here because they're independent of the user input
+    container_in_dir = Path("/data")
+    container_out_dir = Path("/out_dir")
+
+    # build singularity bind mount paths (to include only test case images without segmentation)
+    # this will result in a very long bind path, unfortunately.
+    bind_str = ""
+    for case in in_dir.iterdir():
+        if not case.is_dir():
+            continue
+        subject_id = case.name
+        t1_path = case / f"{subject_id}_t1.nii.gz"
+        t1c_path = case / f"{subject_id}_t1ce.nii.gz"
+        t2_path = case / f"{subject_id}_t2.nii.gz"
+        fl_path = case / f"{subject_id}_flair.nii.gz"
+        bind_str += (
+            f"{t1_path}:{container_in_dir.joinpath(*t1_path.parts[-2:])}:ro,"
+            f"{t1c_path}:{container_in_dir.joinpath(*t1c_path.parts[-2:])}:ro,"
+            f"{t2_path}:{container_in_dir.joinpath(*t2_path.parts[-2:])}:ro,"
+            f"{fl_path}:{container_in_dir.joinpath(*fl_path.parts[-2:])}:ro,"
+        )
+    assert "_seg.nii.gz" not in bind_str, "Container should not have access to segmentation files!"
+    
+    bind_str += f"{out_dir}:/{container_out_dir}:rw"
+    print(f"The bind path string is in total {len(bind_str)} characters long.")
+    os.environ["SINGULARITY_BINDPATH"] = bind_str
+
+    print("\nRunning container...")
+
+    ret = ""
+    try:
+        start_time = time.monotonic()
+
+        singularity_str = (
+            f"singularity run -C --writable-tmpfs --net --network=none --nv"
+            f" {sif} -i {container_in_dir} -o {container_out_dir}"
+        )
+        
+        print(singularity_str)
+        ret = subprocess.run(
+            shlex.split(singularity_str),
+            timeout=timeout_case * num_cases,
+            check=True
+        )
+        end_time = time.monotonic()
+    except subprocess.TimeoutExpired:
+        print(f"Timeout of {timeout_case * num_cases} reached (for {num_cases} cases)."
+              f" Aborting...")
+        exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Running container failed:")
+        raise e
+        # I re-raise exceptions here, because they would indicate that something is wrong with the submission
+    print(f"Execution time of the container: {end_time - start_time:0.2f} s")
+
 
 if __name__ == "__main__":
 
@@ -40,63 +98,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    TIME_PER_CASE = args.timeout   # seconds
-    sif_file = args.sif_file
-    input_dir = Path(args.input_dir)
-    output_dir = Path(args.output_dir)
+    run_container(
+        args.sif_file,
+        in_dir=Path(args.input_dir),
+        out_dir=Path(args.output_dir),
+        timeout_case=args.timeout
+    )
 
-    container_indir = Path("/data")
-    container_outdir = Path("/out_dir")
-
-    num_cases = len(list(input_dir.iterdir()))
-
-    # build singularity bind mount paths (to include only test case images without segmentation)
-    # this will result in a very long bind path, unfortunately.
-    bind_str = ""
-    for case in input_dir.iterdir():
-        if not case.is_dir():
-            continue
-        subject_id = case.name
-        t1_path = case / f"{subject_id}_t1.nii.gz"
-        t1c_path = case / f"{subject_id}_t1ce.nii.gz"
-        t2_path = case / f"{subject_id}_t2.nii.gz"
-        fl_path = case / f"{subject_id}_flair.nii.gz"
-        bind_str += (
-            f"{t1_path}:{container_indir.joinpath(*t1_path.parts[-2:])}:ro,"
-            f"{t1c_path}:{container_indir.joinpath(*t1c_path.parts[-2:])}:ro,"
-            f"{t2_path}:{container_indir.joinpath(*t2_path.parts[-2:])}:ro,"
-            f"{fl_path}:{container_indir.joinpath(*fl_path.parts[-2:])}:ro,"
-        )
-    assert "_seg.nii.gz" not in bind_str, "Container should not have access to segmentation files!"
     
-    bind_str += f"{output_dir}:/{container_outdir}:rw"
-    print(f"The bind path string is in total {len(bind_str)} characters long.")
-    os.environ["SINGULARITY_BINDPATH"] = bind_str
-
-    print("\nRunning container...")
-
-    ret = ""
-    try:
-        start_time = time.monotonic()
-
-        singularity_str = (
-            f"singularity run -C --writable-tmpfs --net --network=none --nv"
-            f" {sif_file} -i {container_indir} -o {container_outdir}"
-        )
-        
-        print(singularity_str)
-        ret = subprocess.run(
-            shlex.split(singularity_str),
-            timeout=TIME_PER_CASE * num_cases,
-            check=True
-        )
-        end_time = time.monotonic()
-    except subprocess.TimeoutExpired:
-        print(f"Timeout of {TIME_PER_CASE * num_cases} reached (for {num_cases} cases)."
-              f" Aborting...")
-        exit(1)
-    except subprocess.CalledProcessError as e:
-        print(f"Running container failed:")
-        raise e
-        # I re-raise exceptions here, because they would indicate that something is wrong with the submission
-    print(f"Execution time of the container: {end_time - start_time:0.2f} s")
--- scripts/test_container.py
+++ scripts/test_container.py
@@ -1,11 +1,11 @@
+"""
+This script can be executed by participants locally to test their container before uploading it for a functionality test
+"""
+
 import argparse
-import os
+import logging
 from pathlib import Path
-import shlex
-import subprocess
-import sys
 import tempfile
-import time
 
 # TODO use logging instead of prints
 
@@ -49,7 +49,7 @@ def is_fets_prediction_folder(pred_path: Path, data_path: Path, algorithm_id: st
     subjects = list(data_path.iterdir())
     predictions = list(pred_path.iterdir())
     if len(subjects) != len(predictions):
-        print(f"Number of patients and predictions does not match!")   # warning
+        logging.warning(f"Number of patients and predictions does not match!")
     # TODO decide how strict we are going to be during evaluation: What happens if more than the required files exist in output dir? check here?
     for case in subjects:
         match_found = False
@@ -60,19 +60,17 @@ def is_fets_prediction_folder(pred_path: Path, data_path: Path, algorithm_id: st
                     match_found = True
                     break
             else:
-                print(f"Found misplaced dir: {pred}. This will not be considered for evaluation!")   # warning
+                logging.warning(f"Found misplaced dir: {pred}. This will not be considered for evaluation!")
         if not match_found:
-            print(f"Could not find a prediction for case {case.name}!")   # error
+            logging.error(f"Could not find a prediction for case {case.name}!")
             error_list.append(case.name)
-    print(f"encountered {len(error_list)} errors. Please check logs")
+    logging.info(f"Encountered {len(error_list)} errors. Please check the logs.")
     return error_list
 
 
 if __name__ == "__main__":
-    # TODO 
-    raise RuntimeError("this code is outdated! Will be updated once the run_submission.py is fixed.")
-
-    print("Testing FeTS singularity image...")
+    # This import works only if this is executed as a script
+    from run_submission import run_container
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -96,19 +94,19 @@ if __name__ == "__main__":
         help="Time budget PER CASE in seconds. Evaluation will be stopped after the total timeout of timeout * n_cases."
     )
     parser.add_argument(
-        "--bind_segs", action="store_true", help="Testing option for binding the whole FeTS data folder (including groundtruth seg.)."
+        "--log_file", default='test.log', required=False, type=str,
+        help="Path where logs should be stored."
     )
-    parser.add_argument(
-        "--test_bindings", action="store_true", help="Testing option for printing the directory content of /data to a file."
-    )
-
     args = parser.parse_args()
 
+    logging.basicConfig(handlers=[logging.FileHandler(args.log_file), logging.StreamHandler()],
+                        encoding='utf-8', level=logging.INFO)
+    logging.info("Testing FeTS singularity image...")
+
+
     TIME_PER_CASE = args.timeout   # seconds
-    sif_file = args.sif_file
+    sif_file = Path(args.sif_file)
     input_dir = Path(args.input_dir)
-    bind_all = args.bind_segs   # TODO remove this option
-    test_ls = args.test_bindings   # TODO remove this option
     
     tmp_dir = None
     if args.output_dir is None:
@@ -118,78 +116,23 @@ if __name__ == "__main__":
     else:
         output_dir = Path(args.output_dir)
 
-    num_cases = len(list(input_dir.iterdir()))
+    # maybe add input dir check here
 
-    # build singularity bind mount paths (to include only test case images without segmentation)
-    # this will result in a very long bind path, unfortunately.
-    if not bind_all:   # default case
-        bind_str = ""
-        container_dir = Path("/data")
-        for case in input_dir.iterdir():
-            if not case.is_dir():
-                continue
-            subject_id = case.name
-            t1_path = case / f"{subject_id}_t1.nii.gz"
-            t1c_path = case / f"{subject_id}_t1ce.nii.gz"
-            t2_path = case / f"{subject_id}_t2.nii.gz"
-            fl_path = case / f"{subject_id}_flair.nii.gz"
-            bind_str += (
-                f"{t1_path}:{container_dir.joinpath(*t1_path.parts[-2:])}:ro,"
-                f"{t1c_path}:{container_dir.joinpath(*t1c_path.parts[-2:])}:ro,"
-                f"{t2_path}:{container_dir.joinpath(*t2_path.parts[-2:])}:ro,"
-                f"{fl_path}:{container_dir.joinpath(*fl_path.parts[-2:])}:ro,"
-            )
-        bind_str += f"{output_dir}:/out_dir:rw"
-    else:
-        bind_str = f"{input_dir}:/data:ro,{output_dir}:/out_dir:rw"
-    print(f"The bind path string is in total {len(bind_str)} characters long.")
-    os.environ["SINGULARITY_BINDPATH"] = bind_str
-
-    print("\nRunning container...")
-
-    ret = ""
-    try:
-        start_time = time.monotonic()
-
-        if not test_ls:   # default case
-            singularity_str = (
-                f"singularity run -C --writable-tmpfs --net --network=none --nv"
-                f" {sif_file} -i /data -o /out_dir"
-            )
-        else:
-            # this is for checking that the segmentations are hidden
-            singularity_str = (
-                f"singularity exec -C --writable-tmpfs --net --network=none --nv"
-                f" {sif_file} ls -aR /data > /out_dir/file_list_container"
-            )
-        ret = subprocess.run(
-            shlex.split(singularity_str),
-            timeout=TIME_PER_CASE * num_cases,
-            check=True
-        )
-        end_time = time.monotonic()
-    except subprocess.TimeoutExpired:
-        print(f"Timeout of {TIME_PER_CASE * num_cases} reached (for {num_cases} cases)."
-              f" Aborting...")
-        exit(1)
-    except subprocess.CalledProcessError:
-        print(f"Running container failed:")
-        print(ret)
-        exit(1)
-    # TODO maybe "except else" block to catch any other exceptions and try to continue evaluation?
-    print(f"Execution time of the container: {end_time - start_time:0.2f} s")
+    # This runs the container in the same way it is done in the testing phase
+    run_container(sif_file, input_dir, output_dir, TIME_PER_CASE)
 
     # check output
     if not is_fets_prediction_folder(output_dir, input_dir, algorithm_id=sif_file.stem):
-        print("Output folder test not passed. Please check messages above. Exiting...")
-        exit(1)
+        # TODO: not sure if the algorithm id will be part of the container name.
+        logging.error("Output folder test not passed. Please error messages in the logs.")
+        # exit(1)
 
-    print("\nEvaluating predictions...")
+    logging.info("Evaluating predictions...")
     # Metrics are calculated in FeTS-CLI
-    # TODO but for testing, it would be good if the participants can do it here.
+    # TODO but for testing, it would be good if the participants can do it here. Ask Sarthak if it can be included here somehow...
 
     if tmp_dir is not None:
+        logging.info("Cleaning up temporary output folder...")
         tmp_dir.cleanup()
-    # TODO maybe clean-up? But maybe it's also better to just wipe the output folder after evaluation of one algorithm is done (in CLI)
 
-    print("Done.")
+    logging.info("Done.")
