74962fd8f25d133121722a6f1f1b673061d1735b
Maximilian Zenk <m.zenk@dkfz-heidelberg.de>
2021-05-31 22:31:12 +0200

    Delete files that should not be released.

--- Task_2/scripts/pull_container.sh
+++
@@ -1,6 +0,0 @@
-# Can maybe even iterate through a file with urls and output filenames...
-# Inputs:
-# 1: project id
-# 2: package name, version, file
-# 3: output path
-curl -K ~/fets_curl.cfg -o $3 "https://gitlab.hzdr.de/api/v4/projects/$1/packages/generic/$2"
